439aadc56a32518765597b90216a5feadaf65c19
CaeJsar <caesarcipher100@gmail.com>
2024-07-28 10:36:30 +0530

    Create Find Smallest Letter Greater Than Target.cpp

---
+++ Binary Search/Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& v, char target) {
+
+        int s=0,e=v.size()-1;
+        int mid;
+        if(target>=v[e])return v[s];
+        while(s<=e){
+            mid = s+(e-s)/2;
+            
+            if(v[mid] > target ){
+                e = mid-1;
+            }
+            else if(v[mid] <= target){
+                s = mid+1;
+            }
+        }
+        return v[s];
+    }
+};
